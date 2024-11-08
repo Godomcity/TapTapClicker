8147f8996d3f57fb9a5d796806bf2e86743da6ed
Godomcity <134502441+Godomcity@users.noreply.github.com>
2024-11-08 20:08:25 +0900

    feat : 오토클릭 추가

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -40,5 +40,4 @@ public class PlayerController : MonoBehaviour
             clickEvent?.Invoke();
         }
     }
-    
 }
---
+++ Assets/Scripts/UI/AutoButton.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoButton : MonoBehaviour
+{
+    private bool isAutoClick = false;
+    private Coroutine coroutine;
+    
+    public void AutoClickButton()
+    {
+        isAutoClick = !isAutoClick;
+       
+        if (coroutine != null)
+        {
+            StopCoroutine(coroutine);
+            coroutine = null;
+        }
+        else
+        {
+            coroutine = StartCoroutine(AutoClick());
+        }
+    }
+
+    IEnumerator AutoClick()
+    {
+        while (true)
+        {
+            Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            RaycastHit2D hit = Physics2D.Raycast(mousePos, Vector2.zero);
+            Debug.Log("클릭클릭");
+            yield return new WaitForSeconds(1);
+        }
+    }
+}
