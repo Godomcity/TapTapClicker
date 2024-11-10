a98563c64ad260d25de2593dd21b912ee4c649ca
Godomcity <134502441+Godomcity@users.noreply.github.com>
2024-11-10 14:40:30 +0900

    chore : 몬스터 데이터 추가

---
+++ Assets/ScriptableObject/MonsterData.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Monster", menuName = "NewMonster")]
+
+public class MonsterData : ScriptableObject
+{
+    [Header("MonsterInfo")]
+    public int health;
+    public GameObject monsterPrefabs;
+}
